577310163185078e41c93d8c0e28dc6872e3051d
Anitha chowdary <130220172+anitha-anii@users.noreply.github.com>
2023-05-28 19:10:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,44 @@
-// Array of API URLs to fetch data from
-const apiUrls = [
-  "https://jsonplaceholder.typicode.com/todos/1",
-  "https://jsonplaceholder.typicode.com/todos/2",
-  "https://jsonplaceholder.typicode.com/todos/3",
-  "https://jsonplaceholder.typicode.com/todos/4",
-  "https://jsonplaceholder.typicode.com/todos/5",
-  "https://jsonplaceholder.typicode.com/todos/6",
-  "https://jsonplaceholder.typicode.com/todos/7",
-  "https://jsonplaceholder.typicode.com/todos/8",
-  "https://jsonplaceholder.typicode.com/todos/9",
-  "https://jsonplaceholder.typicode.com/todos/10",
-];
+ const apiUrls = [
+      "https://jsonplaceholder.typicode.com/todos/1",
+      "https://jsonplaceholder.typicode.com/todos/2",
+      "https://jsonplaceholder.typicode.com/todos/3",
+      "https://jsonplaceholder.typicode.com/todos/4",
+      "https://jsonplaceholder.typicode.com/todos/5",
+      "https://jsonplaceholder.typicode.com/todos/6",
+      "https://jsonplaceholder.typicode.com/todos/7",
+      "https://jsonplaceholder.typicode.com/todos/8",
+      "https://jsonplaceholder.typicode.com/todos/9",
+      "https://jsonplaceholder.typicode.com/todos/10",
+    ];
 
-// You can write your code here
+    async function fetchDataWithPromises() {
+      const startTime = Date.now();
+
+      const allPromises = apiUrls.map(url => fetch(url));
+      const anyPromise = Promise.any(allPromises);
+
+      try {
+        const responses = await Promise.all(allPromises);
+        const allEndTime = Date.now();
+        const timetakenbyall = allEndTime - startTime;
+        const tdall = document.getElementById('output-all');
+        tdall.innerText = timetakenbyall;
+        console.log("Promise.all - Time taken:", timetakenbyall, "ms");
+      } catch (error) {
+        console.error("Promise.all - Error:", error);
+      }
+
+      try {
+        const response = await anyPromise;
+        const anyEndTime = Date.now();
+        const timetakenbyany = anyEndTime - startTime;
+        const tdany = document.getElementById('output-any');
+        tdany.innerText = timetakenbyany;
+        console.log("Promise.any - Time taken:", timetakenbyany, "ms");
+      } catch (error) {
+        console.error("Promise.any - Error:", error);
+      }
+    }
+
+    fetchDataWithPromises();
+
